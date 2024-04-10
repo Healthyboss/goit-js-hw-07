709bd567f5f3152936efda2367c1663b78634fc6
Joanna <joanna_pinczuk@hotmail.com>
2024-04-10 17:56:50 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,21 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector(".class");
-
+const gallery = document.querySelector(".gallery");
 for (let i = 0; i < galleryItems.length; i++) {
-  const galleryItem = `
-    <li class="gallery__item">
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `<li class="gallery__item">
       <a class="gallery__link" href="${galleryItems[i].original}">
         <img
           class="gallery__image"
-          src="${galleryItems[i].preview}
-          data-source="${galleryItems[i].original}
+          src="${galleryItems[i].preview}"
+          data-source="${galleryItems[i].original}"
           alt="${galleryItems[i].description}"
         />
       </a>
-    </li>`;
-  gallery.insertAdjacentHTML("beforeend", galleryItem);
+    </li>`
+  );
 }
+
 console.log(galleryItems);
 console.log(gallery);
